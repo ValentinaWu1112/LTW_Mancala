25b400bbe36d62cb5c833dcb698407844052d989
Pedro Pinheiro <up201906788@fe.up.pt>
2022-01-19 23:00:18 +0000

    still can't fix it :(

--- script/canvas.js
+++ script/canvas.js
@@ -11,29 +11,22 @@ gc.strokeStyle = "dark grey";
 gc.fillStyle = "#c78e47";
 gc.font = 'bold 50px Aclonica';
 
-(function loop() {
-    setTimeout(animation(), 4000);
-
-})();
-
-function animation() {
-    gc.clearRect(x, 0, 300, 100);
-    draw();
-}
 /*
 inspired from this:
 https://stackoverflow.com/questions/29911143/how-can-i-animate-the-drawing-of-text-on-a-web-page 
 */
-function draw() {
+(function draw() {
+    //setTimeout(gc.clearRect(0, 0, 300, 100), 7000);
     gc.setLineDash([dashLen - dashOffset, dashOffset - speed]);
     dashOffset -= speed;
     gc.strokeText(txt[i], x, 50);
 
-    if (dashOffset > 0) requestAnimationFrame(draw);
-    else {
+    if (dashOffset > 0) {
+        requestAnimationFrame(draw);
+    } else {
         gc.fillText(txt[i], x, 50);
         dashOffset = dashLen;
         x += gc.measureText(txt[i++]).width + gc.lineWidth * Math.random();
         if (i < txt.length) requestAnimationFrame(draw);
     }
-}
+})();
